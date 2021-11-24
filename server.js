2492cc09171d694579736dc69e6b65d3f1c2d272
Acdumond <email@andrew-dumond.com>
2021-11-24 12:40:07 -0500

    added hello world

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routes = require("./routes");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
+const { get } = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,8 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
-app.use(routes);
 
+app.use(routes);
+app.get("/", (req, res) => res.send("Hello World"));
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
