fe8befe58d97c8ad378f131dfababbcbf59d7502
Acdumond <email@andrew-dumond.com>
2021-11-28 16:54:32 -0500

    edited linked calendar.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const apiRoutes = require('./api');
+const apiRoutes = require("./api");
 
-router.use('/api', apiRoutes);
+router.use("/api", apiRoutes);
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,10 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // turn on routes
 app.engine("handlebars", hbs.engine);
