229fe450d03329333839b124f1f5bec5124e7412
Acdumond <email@andrew-dumond.com>
2021-11-28 17:07:54 -0500

    edited linked calendar.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const routes = require("./routes");
 const sequelize = require("./config/connection");
