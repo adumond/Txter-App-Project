4b1a85239eeb9c4b736051b4025c12f967f3ab63
Acdumond <email@andrew-dumond.com>
2021-11-28 16:55:24 -0500

    edited linked calendar.js

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,4 @@ const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 module.exports = router;
